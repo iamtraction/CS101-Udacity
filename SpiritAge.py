ff706c2a2ccb7cefa0ac71e21a7951312013ee08
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-07-26 04:19:31 +0530

    Quiz: Spirit Age

---
+++ SpiritAge.py
@@ -0,0 +1,8 @@
+# Write python code that defines the variable
+# age to be your age in years, and then prints
+# out the number of days you have been alive.
+
+age = 15
+days = age * 365.25
+
+print days
