49cd288ef046f36c78c9461573f758e4259b8110
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-07-26 03:45:55 +0530

    Improved SpeedOfLight.py

--- SpeedOfLight.py
+++ SpeedOfLight.py
@@ -5,4 +5,9 @@ speed_of_light = 299792458   # meters per second
 centimeters = 100            # one meter is 100 centimeters
 nanosecond = 1.0/1000000000  # one billionth of a second
 
-print speed_of_light * centimeters * nanosecond
+# Nanostick: A stick that's lenght is same as the length
+# light travels in one nanosecond.
+# Grace Hopper used to hold that.
+nanostick = speed_of_light * centimeters * nanosecond
+
+print nanostick
