ca11b31a498ab2cd922c07895874ef14a6582f30
Sankarsan Kampa <snkrsn.kampa@gmail.com>
2017-07-28 22:21:38 +0530

    Improvements & Fixes DaysBetweenDates

--- Quiz/DaysBetweenDates.py
+++ Quiz/DaysBetweenDates.py
@@ -16,6 +16,12 @@ def isLeapYear(year):
     return False
 
 def daysInMonth(year, month):
+    if month == 1 or month == 3 or month == 5 or month == 7 \
+        or month == 8 or month == 10 or month == 12:
+            return 31
+    if month == 2:
+        # Not accounting for leap-year, yet
+        return 28
     return 30
 
 def nextDate(year, month, date):
@@ -39,7 +45,7 @@ def dateIsBefore(year1, month1, day1, year2, month2, day2):
             return day1 < day2
     return False
 
-def daysBetweenDays(year1, month1, day1, year2, month2, day2):
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1 and year2/month2/day2."""
     assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
     days = 0
@@ -50,9 +56,11 @@ def daysBetweenDays(year1, month1, day1, year2, month2, day2):
 
 def test():
     # Tests with 30-day months
-    assert daysBetweenDays(2014, 1, 1, 2014, 1, 1) == 0
-    assert daysBetweenDays(2014, 1, 1, 2014, 1, 2) == 1
-    assert nextDate(2014, 1, 1) == (2014, 1, 2)
+    assert daysBetweenDates(2014, 1, 1, 2014, 1, 1) == 0
+    assert daysBetweenDates(2014, 1, 1, 2014, 1, 2) == 1
+    assert daysBetweenDates(2014, 1, 1, 2015, 1, 1) == 365
+    assert nextDate(2014, 1, 30) == (2014, 1, 31)
+    assert nextDate(2014, 2, 28) == (2014, 3, 1)
     assert nextDate(2014, 4, 30) == (2014, 5, 1)
     assert nextDate(2014, 12, 31) == (2015, 1, 1)
     print "Tests passed successfully!"
